410091c7a72ddb761532f1dd436977e886552c4d
Emanuel Borges <ebbmango@tuta.io>
2025-11-28 12:16:29 +0100

    placeholdr landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,12 @@
-<h1>Hello, world!</h1>
+<div class="flex flex-col h-dvh w-full items-center justify-center pb-20">
+    <h1 class="text-2xl">Welcome!</h1>
+    <ul>
+        <li class="flex gap-1">
+            <span>{">"}</span>
+            <a href="/quote-slicer-docs" class="text-blue-500 hover:text-blue-700">
+                Quote Slicer Docs
+            </a>
+        </li>
+    </ul>
+
+</div>
