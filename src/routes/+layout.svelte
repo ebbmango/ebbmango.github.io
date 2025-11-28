f902f029bd8967a903641624b4d4a9044be1c518
Emanuel Borges <ebbmango@tuta.io>
2025-11-28 11:26:31 +0100

    Add prerendering and base path config

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
-	
+	export const prerender = true;
+
 	let { children } = $props();
+
 </script>
 
 <svelte:head>
