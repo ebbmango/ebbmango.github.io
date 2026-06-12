27b8df985a8ebc48fbcb3f461c73271ee72d7ddd
Emanuel Borges <ebbmango@tuta.io>
2026-06-12 16:44:36 +0200

    feat: activate Quote//Slicer tile, reorder landing page links
    - Quote//Slicer tile now links to live app (was loading placeholder)
    - Reorder tiles: Quote//Slicer, Docs, Hanzi//Slicer
    - Mark Hanzi//Slicer as coming soon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,11 +3,27 @@
 
 	const quoteSolid = icons['square-quote'].sharp.solid;
 	const quoteLight = icons['square-quote'].sharp.regular;
-    const hanziSolid = icons['language'].sharp.solid;
+	const hanziSolid = icons['language'].sharp.solid;
 </script>
 
 <div class="flex h-dvh w-full flex-col items-center justify-center">
 	<ul class="flex flex-col gap-6">
+		<li>
+			<a
+				class="link group opacity-75 duration-300 hocus:opacity-90"
+				href="https://ebbmango.github.io/quote-slicer"
+			>
+				<svg class="h-18 w-18 shrink-0 fill-current" viewBox={quoteLight['view-box']}>
+					{#each quoteLight.paths as path}
+						<path d={path} />
+					{/each}
+				</svg>
+				<div class="link-text duration-300 group-hocus:translate-x-1">
+					<span class="uppercase">Quote//Slicer</span>
+					<span>Work in Progress 🚧</span>
+				</div>
+			</a>
+		</li>
 		<li>
 			<a
 				class="link group opacity-75 duration-300 hocus:opacity-90"
@@ -24,20 +40,8 @@
 				</div>
 			</a>
 		</li>
+
 		<li>
-			<div class="link opacity-30">
-				<svg class="h-18 w-18 shrink-0 fill-current" viewBox={quoteLight['view-box']}>
-					{#each quoteLight.paths as path}
-						<path d={path} />
-					{/each}
-				</svg>
-				<div class="link-text">
-					<span class="uppercase">Quote//Slicer</span>
-					<span>Loading...</span>
-				</div>
-			</div>
-		</li>
-        		<li>
 			<div class="link opacity-30">
 				<svg class="h-18 w-18 shrink-0 fill-current" viewBox={hanziSolid['view-box']}>
 					{#each hanziSolid.paths as path}
@@ -46,7 +50,7 @@
 				</svg>
 				<div class="link-text">
 					<span class="uppercase">Hanzi//Slicer</span>
-					<span>Loading...</span>
+					<span>Coming soon</span>
 				</div>
 			</div>
 		</li>
